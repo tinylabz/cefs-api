fe5eec10eab1b3c52fa788f951500be11f2b538a
ian balijawa <ianbalijawa16@gmail.com>
2023-06-29 18:56:55 +0300

    add conditional return to uri

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.use(rateLimiter);
 app.disable("X-Powered-By");
 app.use(cors());
 app.use(helmet());
-app.use(morgan("combined"));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
